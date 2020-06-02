e24ee6c901e597fc348f05ed971bbea5d7501f60
jongha-shin <smilejohn@hanmail.net>
2020-06-03 00:08:30 +0900

    clone complete

---
+++ BlazorVIsualAcademy/BlazorVIsualAcademy/Pages/Portfolios/PortfolioWebsite.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorVIsualAcademy.Pages.Portfolios
+{
+    public partial class PortfolioWebsite
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            
+        }
+
+        protected void GoToDotNetKorea()
+        {
+            //NavigationManager.NavigateTo("https://www.naver.com");
+            NavigationManager.NavigateTo("/Index");
+        }
+
+    }
+}
