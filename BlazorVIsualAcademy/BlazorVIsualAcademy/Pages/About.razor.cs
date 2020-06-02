62fcc2915d2bcee19318fb8d13b0d1ea0950a125
jongha-shin <smilejohn@hanmail.net>
2020-06-02 23:16:24 +0900

    Blazor 컴포넌트구조 및 코드비하인드 파일생성, 이벤트핸들러 추가

---
+++ BlazorVIsualAcademy/BlazorVIsualAcademy/Pages/About.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorVIsualAcademy.Pages
+{
+    public partial class About : ComponentBase
+    {
+        public string Title = "정보";
+
+        private string subTitle = "사이트 정보";
+
+        public string SubTitle
+        {
+            get { return subTitle; }
+            set { subTitle = value; }
+        }
+
+        // 페이지 로드 시 이벤트핸들러
+        protected override void OnInitialized() 
+        {
+            subTitle = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
---
+++ BlazorVIsualAcademy/BlazorVIsualAcademy/Pages/Services.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorVIsualAcademy.Pages
+{
+    public partial class Services
+    {
+    public string title { get; set; } = "서비스";
+        protected override void OnInitialized()
+        {
+        }
+    }
+}
