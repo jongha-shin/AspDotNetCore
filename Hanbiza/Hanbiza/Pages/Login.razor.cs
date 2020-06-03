8c6a9a46ece30e315f6cb2302c6622e78500905c
jongha-shin <smilejohn@hanmail.net>
2020-06-03 16:57:56 +0900

    2020-06-03 5:00 Hanbiza

---
+++ Hanbiza/Hanbiza/Pages/Login.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hanbiza.Pages
+{
+    public partial class Login
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        protected override void OnInitialized()
+        {
+            
+        }
+
+        protected void Login_Click()
+        {
+            NavigationManager.NavigateTo("/Main");
+        }
+    }
+}
