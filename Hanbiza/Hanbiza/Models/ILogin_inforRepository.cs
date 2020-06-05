5b20adb7d6311abc289e01a3266c99ebedd93d02
jongha-shin <smilejohn@hanmail.net>
2020-06-05 18:11:58 +0900

    asdf

--- Hanbiza/Hanbiza/Models/ILogin_inforRepository.cs
+++ Hanbiza/Hanbiza/Models/ILogin_inforRepository.cs
@@ -2,8 +2,9 @@
 {
     public interface ILogin_inforRepository
     {
-        //Login_infor1 GetLogin_Infor(Login_infor1 login_Infor);
         LoginInfor GetLogin_Infor(LoginInfor login_Infor);
+
+        void SaveCookie(LoginInfor login_Infor);
 
     }
 }
--- Hanbiza/Hanbiza/Models/Login_inforRepository.cs
+++ Hanbiza/Hanbiza/Models/Login_inforRepository.cs
@@ -20,15 +20,21 @@ namespace Hanbiza.Models
         public LoginInfor GetLogin_Infor(LoginInfor login_infor)
         {
             System.Console.WriteLine("GetLogin_Infor(): "+ login_infor.LoginId + " / "+ login_infor.PassW);
-            //var loginUser = from user in dbContext.LoginInfor
-            //                where user.LoginId == "01011111111"
-            //                select user.StaffName;
-            // SELECT StaffName FROM Login_infor WHERE LoginId = '01011111111'
+            var loginUser = from user in dbContext.LoginInfor
+                            where user.LoginId == login_infor.LoginId
+                            select new 
+                            {   
+                                user.Dname, 
+                                user.BizNum, 
+                                user.CompanyName, 
+                                user.LoginId, 
+                                user.StaffId, 
+                                user.StaffName
+                            };
             
-            //var loginUser = dbContext.LoginInfor.FromSqlRaw("SELECT StaffName FROM Login_infor WHERE LoginId = '01011111111'").ToList();
-            var loginUser = dbContext.LoginInfor.FromSqlRaw("SELECT * FROM Login_infor WHERE (LoginId = '" + login_infor.LoginId + "') AND PwdCompare('" + login_infor.PassW + "', PassW) = 1").ToList();
+            //var loginUser = dbContext.LoginInfor.FromSqlRaw("SELECT SEQID, Dname, BizNum, CompanyName, StaffName, StaffID, loginID, JoinDay, State, Regdate, Bigo, BBuseo, MBuseo, Buseo FROM Login_infor WHERE LoginId = '01011111111'").ToList();
+            //var loginUser = dbContext.LoginInfor.FromSqlRaw("SELECT * FROM Login_infor WHERE (LoginId = '" + login_infor.LoginId + "') AND PwdCompare('" + login_infor.PassW + "', PassW) = 1").ToList();
 
-            // (loginID = '" & loginID & "') AND PwdCompare('" & LCase(PassW) & "', passw) = 1
             //IEnumerable<Login_infor1> loginUser = from user in dbContext.Login_infor
             //                                      where user.loginID == login_infor.loginID
             //                                          && user.PassW == login_infor.PassW
@@ -42,15 +48,17 @@ namespace Hanbiza.Models
             //                                          StaffName = user.StaffName
             //                                      };
 
-
             foreach (var item in loginUser)
             {
                 System.Console.WriteLine(item.ToString());
             }
             
-
-            
             return (LoginInfor)loginUser;
+        }
+
+        public void SaveCookie(LoginInfor login_infor)
+        {
+
         }
     }
 }
