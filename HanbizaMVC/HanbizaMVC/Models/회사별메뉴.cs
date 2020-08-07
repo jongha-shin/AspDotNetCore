16c796479f405746a779996892b4c8e0b37424f5
jongha-shin <smilejohn@hanmail.net>
2020-08-07 17:02:30 +0900

    css update

--- HanbizaMVC/HanbizaMVC/Models/회사별메뉴.cs
+++ HanbizaMVC/HanbizaMVC/Models/회사별메뉴.cs
@@ -11,13 +11,15 @@ namespace HanbizaMVC.Models
         public string OT신청 { get; set; }
         public string 휴가신청 { get; set; }
         public string 휴가결재 { get; set; }
+        public string 기타결재 { get; set; }
         public string 연차보기 { get; set; }
         public string 급여명세서 { get; set; }
         public string 확인서명 { get; set; }
         public string 내문서 { get; set; }
         public string 비밀번호변경 { get; set; }
         public string 로그아웃 { get; set; }
-        public string 기타신청 { get; set; }
+        //public string 기타신청 { get; set; }
+        public string 신청 { get; set; }
         public string 인사평가 { get; set; }
     }
 }
